5f461eb06e538e930040c4c75dbfb55ec6aab75d
Kacper <35163@studentwsei.pl>
2021-03-17 16:35:08 +0100

    zadanie 9.1

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -33,6 +33,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
         Scanner input = new Scanner(System.in);
         int num1=0;
         int num2=0;
